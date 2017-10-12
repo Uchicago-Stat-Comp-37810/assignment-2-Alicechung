077fa4c98ee85d2a26665c376223a9a805a52e63
Alice Chung <alicechung@cs.uchicago.edu>
2017-10-12 18:34:54 -0500

    Part1.4
    Move all the function definitions out of the main script into a
    separate file, modify your main script so that it calls that separate
    file using the source() command. Commit.

---
+++ HW2-MHmainscript.R
@@ -0,0 +1,29 @@
+setwd("~/Desktop/2017 FALL/STAT37810/assignment-2-Alicechung/")
+source("HW2-allfunctions.R")
+
+trueA <- 5
+trueB <- 0
+trueSd <- 10
+sampleSize <- 31
+
+# create independent x-values 
+x <- (-(sampleSize-1)/2):((sampleSize-1)/2)
+# create dependent values according to ax + b + N(0,sd)
+y <-  trueA * x + trueB + rnorm(n=sampleSize,mean=0,sd=trueSd)
+
+plot(x,y, main="Test Data")
+
+# Example: plot the likelihood profile of the slope a
+slopelikelihoods <- lapply(seq(3, 7, by=.05), slopevalues)
+plot (seq(3, 7, by=.05), slopelikelihoods , type="l", xlab = "values of slope parameter a", ylab = "Log likelihood")
+
+startvalue = c(4,0,10)
+chain = run_metropolis_MCMC(startvalue, 10000)
+
+burnIn = 5000
+acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
+
+### Summary: #######################
+Summary(chain, burnIn,trueA,trueB,trueSd)
+# for comparison:
+summary(lm(y~x))
